efbede92c8798ab8645e8eb3ecf429ef44f8d6fe
jmborin <jmborin@umich.edu>
2025-02-22 11:37:26 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [text, setText] = useState("I'm bored");
+  const buttonStuff = () => {
+    if (text == "I'm bored") {
+      setText("DRINK");
+    } else {
+      setText("I'm bored");
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="button" onClick={buttonStuff}>
+          {text}
+        </button>
       </header>
     </div>
   );
